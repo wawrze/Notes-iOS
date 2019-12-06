59c903cdda05ed01f979f3e9c6a3da544d577204
mw <wawrze@mws-iMac.local>
2019-12-06 22:49:34 +0100

    view connected to code

--- Notes/ViewController.swift
+++ Notes/ViewController.swift
@@ -8,18 +8,69 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate, UITextViewDelegate {
 
+    // MARK: Properties
+    @IBOutlet weak var titleInput: UITextField!
+    @IBOutlet weak var bodyInput: UITextView!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    //@IBOutlet weak var googleSection: UIStackView!
+    @IBOutlet weak var googleCheckBox: CheckBox!
+    
+    //@IBOutlet weak var securitySection: UIStackView!
+    @IBOutlet weak var securityCheckBox: CheckBox!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        titleInput.delegate = self
+        bodyInput.delegate = self
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // MARK: UITextFieldDelegate
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
     }
+    
+    // MARK: UITextViewDelegate
+    func textViewShouldEndEditing(_ textView: UITextView) -> Bool {
+        textView.resignFirstResponder()
+        return true
+    }
+    
+    // MARK: Actions
+    @IBAction func cameraButton(_ sender: UIButton) {
 
+    }
+    
+    @IBAction func microphoneButton(_ sender: UIButton) {
 
+    }
+    
+    @IBAction func addButton(_ sender: UIButton) {
+        let title = "title: " + titleInput.text!
+        NSLog(title)
+        let body = "body: " + bodyInput.text
+        NSLog(body)
+        if (securityCheckBox.isChecked) {
+            NSLog("security checked")
+        } else {
+            NSLog("security unchecked")
+        }
+        if (googleCheckBox.isChecked) {
+            NSLog("google checked")
+        } else {
+            NSLog("google unchecked")
+        }
+        let date = datePicker.date
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .full
+        dateFormatter.timeStyle = .full
+        var dateString = "date: "
+        dateString += dateFormatter.string(from: date)
+        NSLog(dateString)
+    }
+    
 }
-

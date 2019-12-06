c919650eb9213c8dd6be02d92d903e482d4d0b74
mw <wawrze@mws-iMac.local>
2019-12-06 19:37:12 +0100

    Initial Commit

---
+++ Notes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Notes
+//
+//  Created by mw on 06.12.2019.
+//  Copyright © 2019 mw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
